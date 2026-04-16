a26c07087cff77fd824584495fbda454f4cfdc91
timur_sukhariev <payeer931@gmail.com>
2026-04-16 17:48:04 +0300

    feat: add background SVG images to App component for enhanced visuals

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,8 +7,13 @@ import { fetchWeather } from "./api/weather";
 function App() {
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
 
+
   return (
-    <div className="flex border border-red-500">
+    <div className="flex min-h-screen overflow-hidden relative">
+      {/* Background images: */}
+      <img className="absolute top-[-600px] 2xl:top-[-550px] right-[-600px] 2xl:right-[-550px] pointer-events-none" src="Ellipse.svg" />
+      <img className="absolute bottom-[-600px] 2xl:bottom-[-550px] left-[-600px] 2xl:left-[-550px] pointer-events-none" src="Ellipse2.svg" />
+
       <Sidebar setWeatherData={setWeatherData} />
       {weatherData ?
         <WeatherSection weatherData={weatherData} /> :
@@ -20,3 +25,5 @@ function App() {
 }
 
 export default App;
+
+
